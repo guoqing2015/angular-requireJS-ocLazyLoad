4477dab01e78829d9eda6cae5e1f1d533dd7d1ee
guoqing2013@163.com <qing1211>
2015-12-13 16:22:48 +0800

    angular-requireJS-ocLazyLoad

---
+++ 3. myDemoApp1/app/app.js
@@ -0,0 +1,53 @@
+define(['angular', 'angular-route', 'oc-lazy-load'], function () {
+
+
+    var app = angular.module('App', ['ngRoute','oc.lazyLoad']);
+
+    app.config(['$ocLazyLoadProvider','$routeProvider',
+        function ($ocLazyLoadProvider,$routeProvider ) {
+
+            $ocLazyLoadProvider.config({
+                loadedModules: ['App'],
+                asyncLoader: require
+            });
+
+            //$urlRouterProvider.otherwise('/home');
+
+            $routeProvider
+
+
+                .when('/home', {
+                    templateUrl: 'views/home/home.html',
+                    resolve: {
+                        load: ['$ocLazyLoad' ,function($ocLazyLoad) {
+                            return $ocLazyLoad.load ({
+                                name: 'home',
+                                files: ['views/home/home.js']
+                            })
+                        }]
+                    }
+                })
+
+                .when('/page1', {
+                    templateUrl: 'views/page1/page1.html',
+                    resolve: {
+                        load: function($ocLazyLoad) {
+                            return $ocLazyLoad.load ({
+                                name: 'page1',
+                                files: ['views/page1/page1.js']
+
+                            });
+                        }
+                    }
+                })
+
+
+        }]);
+
+    app.bootstrap = function () {
+        angular.bootstrap(document, ['App']);
+    };
+
+
+    return app;
+});
